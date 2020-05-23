4883b00437a91ab5a84d544a8e505e82873893bd
chase <chase.isabelle@vestiairecollective.com>
2020-05-23 14:00:01 +0200

    Revert "added stopwatch stuff"
    This reverts commit faec18e91e854f1aaea33bd11911916d3e96f773.

--- example/main.go
+++ example/main.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"github.com/chaseisabelle/histo"
 	"time"
 )
 
 func main() {
-	his, err := histo.New("histo_test_latency_seconds", "bla bla bla", []string{
+	histo, err := histo.New("histo_test_latency_seconds", "bla bla bla", []string{
 		"foo",
 	}, nil)
 
@@ -15,29 +14,11 @@ func main() {
 		panic(err)
 	}
 
-	//////////////////////
-	// using the stopwatch
-	//////////////////////
-	his.Start()
-
-	// do something
-
-	his.Stop()
-	his.Record("bar")
-
-	println(fmt.Sprintf("took this many seconds %f", his.Duration()))
-
-	//////////////////////////
-	// using your own duration
-	//////////////////////////
-
 	start := time.Now()
 
 	// do something
 
 	dur := time.Since(start).Seconds()
 
-	his.Observe(dur, "bar")
-
-	println(fmt.Sprintf("took this many seconds %f", his.Duration()))
+	histo.Observe(dur, "bar")
 }
--- histo.go
+++ histo.go
@@ -1,20 +1,15 @@
 package histo
 
-import (
-	"github.com/prometheus/client_golang/prometheus"
-	"time"
-)
+import "github.com/prometheus/client_golang/prometheus"
 
 type Histo struct {
-	vector   *prometheus.HistogramVec
-	start    time.Time
-	duration time.Duration
+	vector *prometheus.HistogramVec
 }
 
 func New(name string, help string, labels []string, buckets []float64) (*Histo, error) {
 	opts := prometheus.HistogramOpts{
-		Name: name,
-		Help: help,
+		Name:    name,
+		Help:    help,
 	}
 
 	if buckets != nil && len(buckets) > 0 {
@@ -41,22 +36,7 @@ func New(name string, help string, labels []string, buckets []float64) (*Histo, 
 	}, nil
 }
 
-func (h *Histo) Observe(value float64, labels ...string) {
+func (h *Histo) Observe(value float64, labels... string) {
 	h.vector.WithLabelValues(labels...).Observe(value)
 }
 
-func (h *Histo) Start() {
-	h.start = time.Now()
-}
-
-func (h *Histo) Stop() {
-	h.duration = time.Since(h.start)
-}
-
-func (h *Histo) Duration() float64 {
-	return h.duration.Seconds()
-}
-
-func (h *Histo) Record(labels ...string) {
-	h.Observe(h.Duration(), labels...)
-}
